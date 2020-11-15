d8b489fed100cd7caeb8d925f455ddab0f86f287
lokhei <lokhei01@gmail.com>
2020-11-15 01:20:58 +0000

    fix links

--- History_Trails/src/main/java/com/SPE/historytrails/History_Trails/WebController.java
+++ History_Trails/src/main/java/com/SPE/historytrails/History_Trails/WebController.java
@@ -1,17 +1,28 @@
 package com.SPE.historytrails.History_Trails;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+//import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WebController {
-    @GetMapping(value = "/hello")
-    public String helloWorld(@RequestParam(defaultValue = "world", required = false) String name, Model model){
-        model.addAttribute("name", name);
+//    @GetMapping(value = "/hello")
+    @RequestMapping("/")
+    public String index(){
+//        model.addAttribute("name", name);
         //First parameter is an attribute name, second parameter is an object to add.
-        return "index" ;
+        return "index";
+    }
+    @GetMapping(value = "/QRCode.html")
+    public String QRCode(){
+        return "QRCode";
+    }
+
+    @GetMapping(value = "/SearchGallery.html")
+    public String search(){
+        return "SearchGallery";
     }
 }
 
