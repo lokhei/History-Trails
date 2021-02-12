dfe6df80a8654f9b7b7025c4a486045077d06316
lokhei <lokhei01@gmail.com>
2021-02-12 16:15:09 +0000

    add responsiveness to sidebar

--- History_Trails/src/main/resources/static/sidenav.js
+++ History_Trails/src/main/resources/static/sidenav.js
@@ -1,7 +1,13 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    let width = screen.width;
+    if ( width < 600) {
+        document.getElementById("mySidenav").style.width = String(width)+"px";
+    }else{
+        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+    }
+
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
