1b929b55be3acf14b61fe40579ab0d35c8a2b9a7
lokhei <lokhei01@gmail.com>
2021-04-23 01:42:23 +0100

    test html rendering

--- History_Trails/src/main/java/com/SPE/historytrails/History_Trails/WebController.java
+++ History_Trails/src/main/java/com/SPE/historytrails/History_Trails/WebController.java
@@ -2,14 +2,17 @@ package com.SPE.historytrails.History_Trails;
 
 import org.springframework.stereotype.Controller;
 //import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 //import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WebController {
 
-    @RequestMapping("/")
+    @GetMapping(value = "/")
     public String index(){ return "index"; }
     @GetMapping(value = "/MShed")
     public String MShed(){
--- History_Trails/src/test/java/com/SPE/historytrails/History_Trails/HistoryTrailsApplicationTests.java
+++ History_Trails/src/test/java/com/SPE/historytrails/History_Trails/HistoryTrailsApplicationTests.java
@@ -1,13 +1,33 @@
 package com.SPE.historytrails.History_Trails;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootTest
 class HistoryTrailsApplicationTests {
 
+	@Autowired
+	WebController webcontroller;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void htmlRendering(){
+		assert(webcontroller.index().equals("index"));
+		assert(webcontroller.MShed().equals("MShed"));
+		assert(webcontroller.BristolArtGallery().equals("BristolArtGallery"));
+		assert(webcontroller.popular().equals("popular"));
+		assert(webcontroller.favourites().equals("favourites"));
+		assert(webcontroller.gallery2().equals("Gallery2"));
+		assert(webcontroller.gallery3().equals("Gallery3"));
+		assert(webcontroller.gallery4().equals("Gallery4"));
+		assert(webcontroller.gallery5().equals("Gallery5"));
+		assert(webcontroller.more().equals("more"));
+
 	}
 
 }
