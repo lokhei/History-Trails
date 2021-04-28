13d0baab97b55078a7851bcd507a2f0fa00130ac
sergiu-avacaritei <s_avacaritei@yahoo.com>
2021-04-28 16:19:11 +0300

    api fully tested

--- History_Trails/src/test/javascript/gallery_spec.js
+++ History_Trails/src/test/javascript/gallery_spec.js
@@ -5,15 +5,83 @@ describe('Testing API...', function() {
         setTimeout(function() {
             // do some stuff
             done();
-        }, 100);
+        }, 500);
     });
-
 
     it('fetches all the objects', async function(done) {
         const result = await fetchObjects();
         expect(result.length).toBe(115);
         done();
-
     });
+
+    it('Gallery 2', async function (done) {
+        const result = await fetchObjects();
+        let objectsGallery2 = [];
+        for (var i = 0; i < 115; i++) {
+            if (result[i].fields.gallery == "Gallery 2") {
+                objectsGallery2.push(result[i]);
+            }
+        }
+        expect(objectsGallery2.length).toBe(31);
+        done();
+    });
+
+
+    it('Gallery 3 - European Old Masters', async function (done) {
+        const result = await fetchObjects();
+        let objectsGallery3 = [];
+        for (var i = 0; i < 115; i++) {
+            if (result[i].fields.gallery == "Gallery 3 - European Old Masters") {
+                objectsGallery3.push(result[i]);
+            }
+        }
+        expect(objectsGallery3.length).toBe(44);
+        done();
+    });
+
+    it('Gallery 4', async function (done) {
+        const result = await fetchObjects();
+        let objectsGallery4 = [];
+        for (var i = 0; i < 115; i++) {
+            if (result[i].fields.gallery == "Gallery 4") {
+                objectsGallery4.push(result[i]);
+            }
+        }
+        expect(objectsGallery4.length).toBe(22);
+        done();
+    });
+
+
+    it('Gallery 5 - Modern Art', async function (done) {
+        const result = await fetchObjects();
+        let objectsGallery5 = [];
+        for (var i = 0; i < 115; i++) {
+            if (result[i].fields.gallery == "Gallery 5 - Modern Art") {
+                objectsGallery5.push(result[i]);
+            }
+        }
+        expect(objectsGallery5.length).toBe(18);
+        done();
+    });
+
+    it('Works all links to additional information', async function (done) {
+        const result = await fetchObjects();
+        let urls = [];
+        for (var i = 0; i < 115; i++) {
+            var url = result[i].fields.link_to_additional_information;
+            if (url) {
+                urls.push(url);
+            }
+        }
+
+        for (var i = 0; i < urls.length; i++) {
+            const request = await fetch(urls[0]);
+            expect(request.status).toBe(200);
+
+        }
+        
+        done();
+
+    })
 
 });
