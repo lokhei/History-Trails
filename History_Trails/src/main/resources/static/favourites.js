287532f5ac2801ebdea5558f502de5c7752020c9
lokhei <lokhei01@gmail.com>
2021-05-06 22:04:33 +0100

    put and fetch to server

--- History_Trails/src/main/resources/static/favourites.js
+++ History_Trails/src/main/resources/static/favourites.js
@@ -1,6 +1,19 @@
 
 
-function fav(id) {
+
+async function fetchLikes(id) {
+    let url = "/objects?id=" + id;
+    try {
+        let res = await fetch(url);
+        return (await res.json()).likes;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+async function fav(id) {
     let y = "#" + id;
     let numLikes;
 
@@ -10,10 +23,55 @@ function fav(id) {
             $(y).removeClass("liked");
             eraseCookie(id);
 
+
+            numLikes = (await fetchLikes(id));
+
+            var toSend = {
+                recordid: id,
+                likes: numLikes - 1
+            };
+
+            $.ajax({
+                type: 'POST',
+                url: "/objects?id=" + id,
+                data: toSend,
+                dataType: "json",
+                success: function (data) {
+                    console.log("Success", data);
+                },
+                error: function (data) {
+                    console.log("Failure", data);
+                },
+            });
+
         } else {
             $(y).html('<i class="heart fas fa-heart"></i>');
             $(y).addClass("liked");
             setCookie(id, '1', 365);
+
+
+
+            numLikes = (await fetchLikes(id));
+
+
+
+            var toSend = {
+                recordid: id,
+                likes: numLikes + 1
+            };
+
+            $.ajax({
+                type: 'POST',
+                url: "/objects?id=" + id,
+                data: toSend,
+                dataType: "json",
+                success: function (data) {
+                    console.log("Success", data);
+                },
+                error: function (data) {
+                    console.log("Failure", data);
+                },
+            });
 
 
         }
