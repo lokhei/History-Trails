2bc56f3bc572b83e9843f460fb77c0ec71b83d00
lokhei <lokhei01@gmail.com>
2021-05-06 22:02:16 +0100

    setup individual pages for each object

--- History_Trails/src/main/java/com/SPE/historytrails/History_Trails/controller/ObjectController.java
+++ History_Trails/src/main/java/com/SPE/historytrails/History_Trails/controller/ObjectController.java
@@ -20,6 +20,22 @@ public class ObjectController {
         return objectService.list();
     }
 
+    @RequestMapping(value="",method = RequestMethod.GET  )
+    @ResponseBody
+    public Objects getLikes(@RequestParam(defaultValue="b2b810de148a48a51ddd54ae124389c6831ec3bc")String id )  {
+        return objectService.listOne(id);
+    }
 
+
+    @RequestMapping(value="",method = RequestMethod.POST )
+    @ResponseBody
+    public Objects updateLikes(@RequestParam(defaultValue="b2b810de148a48a51ddd54ae124389c6831ec3bc")String id, Objects newobject )  {
+        Objects object = objectService.listOne(id);
+        object.setLikes(newobject.getLikes());
+        objectService.save(object);
+
+
+        return objectService.listOne(id);
+    }
 
 }
